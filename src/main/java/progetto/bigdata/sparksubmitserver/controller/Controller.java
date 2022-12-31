9222a90d794e74a6e2ef87f48d9c865c7633beaf
Nicola <nicolagabriele99@gmail.com>
2022-12-31 10:54:49 +0100

    aggiunta delle query CoppieHotel_NegReviews e CoppieHotel_PosReviews

--- src/main/java/progetto/bigdata/sparksubmitserver/controller/Controller.java
+++ src/main/java/progetto/bigdata/sparksubmitserver/controller/Controller.java
@@ -1,9 +1,6 @@
 package progetto.bigdata.sparksubmitserver.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import progetto.bigdata.Constants;
 import progetto.bigdata.sparkjobexecutor.models.*;
 import progetto.bigdata.sparkjobexecutor.query.*;
@@ -18,59 +15,60 @@ import java.util.List;
 public class Controller {
 
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/geoDataHotelsInNation")
     public @ResponseBody List<GeoDataClass> geodataHotelsInNation(@RequestParam String nation){
         String[] params = {nation};
         Constants.deleteResultDir();
         return GeoDataHotelsInNation.ISTANCE.compute(params);
     }
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/wordCountPositive")
     public @ResponseBody List<WordCountItem> wordCountPositive(){
         Constants.deleteResultDir();
         return WordCountPositive.INSTANCE.compute(null);
 
     }
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/wordCountNegative")
     public @ResponseBody List<WordCountItem> wordCountNegative(){
         Constants.deleteResultDir();
         return WordCountNegative.INSTANCE.compute(null);
     }
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/coppieHotelPunteggioMedioPerNazione")
     public @ResponseBody List<CoppiaHotelPunteggioMedio> coppieHotelPunteggioMedioPerNazione(@RequestParam String nation){
         String[] params = {nation};
         Constants.deleteResultDir();
         return CoppieHotelPunteggioMedioPerNazione.INSTANCE.compute(params);
     }
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/recensioniHotelDallaData")
     public @ResponseBody List<String> recensioniHotelDallaData(@RequestParam String hotel, @RequestParam String days){
         String[] params = {hotel, days};
         Constants.deleteResultDir();
         return RecensioniHotelDallaData.INSTANCE.compute(params);
     }
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/coppieHotel_NegReviews")
     public @ResponseBody List<CoppiaHotelNumRecensioni> coppieHotel_NegReviews(){
         Constants.deleteResultDir();
         return CoppieHotel_NegReviews.INSTANCE.compute(null);
     }
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/coppieHotel_PosReviews")
     public @ResponseBody List<CoppiaHotelNumRecensioni> coppieHotel_PosReviews(){
         Constants.deleteResultDir();
         return CoppieHotel_PosReviews.INSTANCE.compute(null);
     }
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/averageScoreFilter")
     public @ResponseBody List<FilteredItem> averageScoreFilter(@RequestParam String punteggio){
         String[] params = {punteggio};
         Constants.deleteResultDir();
         return AverageScoreFilter.INSTANCE.compute(params);
     }
-
+    @CrossOrigin(origins = "*")
     @GetMapping("/reviewsNumberFilter")
     public @ResponseBody List<ReviewsNumberItem> reviewsNumberFilter(@RequestParam String num){
         String[] params = {num};
